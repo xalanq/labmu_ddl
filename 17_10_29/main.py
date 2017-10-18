59a427b4f2340edc0051854b6e34a815676a57a3
xalanq <xalanq@gmail.com>
2017-10-18 18:34:21 +0800

    ddl2 - 2 with bugs

--- 17_10_29/main.py
+++ 17_10_29/main.py
@@ -3,18 +3,30 @@ from flask import render_template
 from flask import jsonify
 from flask import url_for
 from flask import request
+import hashlib
+import os
 
 app = Flask(__name__)
 
-user_info = {}
+user_auth = dict()
+pass_salt = str(os.urandom(23333))
+
+
+def my_password_hash(password):
+	return hashlib.sha512((password + pass_salt).encode('utf-8')).hexdigest()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		return render_template('index.html')
+		user_name = request.form.get('username', 'null')
+		user_pass = my_password_hash(request.form.get('password', 'null'))
+		if user_auth.get(user_name) == user_pass:
+			return jsonify(name=user_name, message="Welcome!!")
+		return render_template('index.html', error="Invalid Username or Password.")
 	else:
-		return render_template('index.html')
+		return render_template('index.html', error=None)
 
 if __name__ == '__main__':
+	user_auth['admin'] = my_password_hash('123')
 	app.run()
